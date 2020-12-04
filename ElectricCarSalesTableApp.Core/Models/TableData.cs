eafc63892559e0d58b5362496b45faa1da62e6ce
Justin Martin <33104455+Ekl1p5e@users.noreply.github.com>
2020-12-03 19:12:27 -0600

    Added table data

---
+++ ElectricCarSalesTableApp.Core/Models/TableData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ElectricCarSalesTableApp.Core.Models
+{
+    public class SalesTableData
+    {
+        private readonly DataTable _table;
+
+        public SalesTableData(DataTable table)
+        {
+            _table = table ?? throw new ArgumentNullException(nameof(table));
+        }
+
+        public IEnumerable<string> ColumnNames => _table.Columns.Cast<DataColumn>().Select(c => c.ColumnName);
+
+        public IEnumerable<object[]> Rows => _table.Rows.Cast<DataRow>().Select(c => c.ItemArray);
+    }
+}
