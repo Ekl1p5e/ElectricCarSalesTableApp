79bc6ffaf5dcfff2045dcbd6fd0080837b171c14
Justin Martin <33104455+Ekl1p5e@users.noreply.github.com>
2020-12-04 12:34:50 -0600

    Calculating columns

--- ElectricCarSalesTableApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ElectricCarSalesTableApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,11 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public salesdata: SalesTableData;
+  public aggregatedata: AggregateData;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<SalesTableData>(baseUrl + 'salestabledata').subscribe(result => {
-      this.salesdata = result;
+      this.aggregatedata = new AggregateData(result);
     }, error => console.error(error));
   }
 }
@@ -18,4 +18,42 @@ export class FetchDataComponent {
 interface SalesTableData {
   columnNames: string[];
   rows: string[][];
+}
+
+class AggregateData {
+  columnNames: string[];
+  rows: string[][];
+
+  columnData: number[][];
+
+  get averages() { return this.columnData.map(c => getAverage(c)); }
+  get medians() { return this.columnData.map(c => getMedian(c)); }
+  get totals() { return this.columnData.map(c => getTotal(c)); }
+
+  constructor(tabledata: SalesTableData) {
+    this.rows = tabledata.rows;
+    this.columnNames = tabledata.columnNames;
+
+    this.columnData = this.rows.map(r => r.slice(1).map(n => [n]))
+      .reduce((baseRow, nextRow) => baseRow.map((x, i) => x.concat(nextRow[i])))
+      .map(values => values.map(s => Number(s)));
+  }
+}
+
+function getAverage(list: number[]) {
+  return list.reduce((base, next) => base + next) / list.length;
+}
+
+function getTotal(list: number[]) {
+  return list.reduce((first, next) => first + next);
+}
+
+function getMedian(list: number[]) {
+  if (list.length === 0) return 0;
+
+  list.sort((a, b) => a - b);
+
+  var middle = Math.floor(list.length / 2);
+
+  return list.length % 2 ? list[middle] : (list[middle - 1] + list[middle]) / 2.0;
 }
