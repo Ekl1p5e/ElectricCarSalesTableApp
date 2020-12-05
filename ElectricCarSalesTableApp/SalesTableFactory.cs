21d702030c0a421a8030217dfce08f30400372a1
Justin Martin <33104455+Ekl1p5e@users.noreply.github.com>
2020-12-04 20:37:20 -0600

    Added comments

--- ElectricCarSalesTableApp/SalesTableFactory.cs
+++ ElectricCarSalesTableApp/SalesTableFactory.cs
@@ -6,17 +6,29 @@ using System.Threading.Tasks;
 
 namespace ElectricCarSalesTableApp
 {
+    /// <summary>
+    /// Class used to create sales data tables
+    /// </summary>
     public class SalesTableFactory : ISalesTableFactory
     {
         private readonly string _filepath;
         private readonly ISalesDataTableLoader _tableLoader;
 
+        /// <summary>
+        /// Constructs and instance of the <see cref="SalesTableFactory"/> class
+        /// </summary>
+        /// <param name="tableLoader">class used to load table</param>
+        /// <param name="filepath">source file path</param>
         public SalesTableFactory(ISalesDataTableLoader tableLoader, string filepath)
         {
             _filepath = !string.IsNullOrWhiteSpace(filepath) ? filepath : throw new ArgumentNullException(nameof(filepath));
             _tableLoader = tableLoader ?? throw new ArgumentNullException(nameof(tableLoader));
         }
 
+        /// <summary>
+        /// Gets table
+        /// </summary>
+        /// <returns>returns sales data table</returns>
         public async Task<SalesTableData> GetTable()
         {
             var table = await Task.Factory.StartNew(() => _tableLoader.GetTable(_filepath));
